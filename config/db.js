5d1e02764d76154fd1a4fd7e831a9b3bd5e4aa47
Lee Jones KE <93858479+Leejoneske@users.noreply.github.com>
2024-10-03 11:54:01 +0300

    Create db.js

---
+++ config/db.js
@@ -0,0 +1,17 @@
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  }
+});
+
+pool.connect((err) => {
+  if (err) {
+    console.error('Error connecting to the database', err.stack);
+  } else {
+    console.log('Connected to the PostgreSQL database');
+  }
+});
+
+module.exports = { pool };
